44442960f104ef1886158935f20a3102af092b7d
kevinconka <kserranov@gmail.com>
2023-09-21 23:39:16 +0200

    minor changes due to usage

--- train-IR.sh
+++ train-IR.sh
@@ -5,7 +5,7 @@ ${HOMEDIR}/miniconda3/envs/AI/bin/python ${HOMEDIR}/GitHub/yolov5-extended/train
     --batch 32 \
     --epochs 100 \
     --data ${HOMEDIR}/GitHub/yolov5-extended/datasets/TRAIN_THERMAL_DATASET_2023_06_2023-08-01/dataset.yaml \
-    --weights runs/train/yolov5n_T16-8_D2306-v0_SC2/weights/best.pt \
+    --weights yolos/yolov5n_T16-8_D2306-v0_SC.pt \
     --name ${MODEL}_T16-8_D2306-v0_9C \
     --hyp ${HOMEDIR}/GitHub/yolov5-extended/data/hyps/hyp.sea-ai-IR.yaml \
     --workers 4 \
--- train.sh
+++ train.sh
@@ -1,9 +1,13 @@
-MODEL=yolov5n6_trained4singleCls
-/home/jigglypuff/anaconda3/envs/yolo/bin/python /home/jigglypuff/GitHub/yolov5-extended/train.py \
+HOMEDIR=/home/kevinserrano
+MODEL=yolov5n6
+${HOMEDIR}/miniconda3/envs/AI/bin/python ${HOMEDIR}/GitHub/yolov5-extended/train.py \
     --img 1280 \
     --batch 32 \
-    --epochs 20 \
-    --data /home/jigglypuff/GitHub/yolov5-extended/datasets/RGB_25_24-04-2023/dataset.yaml \
-    --weights ${MODEL}.pt \
-    --name RGB_25k_fineTuneSnglCls_modifiedTrainVal_${MODEL} \
-    --hyp /home/jigglypuff/GitHub/yolov5-extended/data/hyps/hyp.sea-ai.yaml \
+    --epochs 50 \
+    --data /home/jigglypuff/GitHub/yolov5-extended/datasets/RGB_25_17-04-2023/dataset.yaml \
+    --weights yolos/yolov5n6_RGB_D2304-v0_SC.pt \
+    --name ${MODEL}_RGB_D2304-v0_9C \
+    --hyp ${HOMEDIR}/GitHub/yolov5-extended/data/hyps/hyp.sea-ai.yaml \
+#    --workers 4 \
+#    --device 0
+#    --single-cls
